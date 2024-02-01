77de67ec9f6ab166d16734a00fdb44a9045f38dd
Rahul <rahuljethwa1411@gmail.com>
2024-02-02 05:01:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = "072800625a2689ba6388288ab593e17f";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchButton.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
